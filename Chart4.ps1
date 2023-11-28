49226187356e4df732027adb7b9b92309721c48b
KekeD0 <152340768+KekeD0@users.noreply.github.com>
2023-11-28 18:01:05 -0500

    Add files via upload

---
+++ Chart4.ps1
@@ -0,0 +1,22 @@
+﻿Start-Transcript
+# Check Folder Exists
+If (Test-Path -Path $env:APPDATA\Microsoft\Templates\Charts) {
+    #Get the Folder object
+    $Folder = Get-Item -Path $env:APPDATA\Microsoft\Templates\Charts
+    Write-Host "Folder already exists." -f Yellow
+} Else {
+    #Create a New Folder   
+    $Folder = New-Item -ItemType Directory -Path $env:APPDATA\Microsoft\Templates\Charts
+    Write-Host "Folder Created Successfully!." -f Green
+
+
+#gets currently logged in user
+$Username = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+
+Copy-Item -Path "Charts\*" -Destination "C:\Users\$Username\AppData\Roaming\Microsoft\Templates\Charts" -Recurse -Force
+
+
+New-Item -Path "C:" -Name "Charts" -ItemType "directory" -Force
+Copy-Item -Path "Charts\*" -Destination "C:\Users\$Username\AppData\Roaming\Microsoft\Templates\Charts" -Recurse -Force
+}
+Stop-Transcript
